4cc31839d19291544f9032a61b3960dec4abd04d
sam <opeyemisamuel222@gmail.com>
2022-02-06 20:14:30 +0100

    work out with update and delete

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -31,20 +31,27 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async(id, updatedItem) => {
+    const response = await fetch(`/feedback/${id}`, { method: "PUT", headers:{
+        'Content-Type': "application/json",
+
+    }, body: JSON.stringify(updatedItem) });
+
+    const data = await response.json()
+
     setFeedback(
       feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
+        item.id === id ? { ...item, ...data } : item
       )
     );
   };
 
   // Delete feedback
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     if (window.confirm("Are you sure you want to delete")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
-    console.log("delete", id);
   };
 
   // Add feedback
